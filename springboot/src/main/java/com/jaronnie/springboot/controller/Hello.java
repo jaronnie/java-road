a05d80973331b00f53a8a9884ae5e286d9134525
jaronnie <jaron@jaronnie.com>
2023-08-10 15:09:53 +0800

    feat(road): update

--- springboot/src/main/java/com/jaronnie/springboot/controller/Hello.java
+++ springboot/src/main/java/com/jaronnie/springboot/controller/Hello.java
@@ -1,13 +1,19 @@
 package com.jaronnie.springboot.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class Hello {
-    @RequestMapping("say")
-    public String say() {
-        return "hello world";
+    @RequestMapping("/say")
+    public JSONObject say() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code",200);
+        jsonObject.put("message", "ok");
+        jsonObject.put("data", "hello world");
+
+        return jsonObject;
     }
 }
