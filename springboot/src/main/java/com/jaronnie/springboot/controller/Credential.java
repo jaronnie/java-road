e0d2970bf0ed5ea6cc0de58870772d3abac1b4f5
jaronnie <jaron@jaronnie.com>
2023-08-17 01:42:20 +0800

    add post

--- springboot/src/main/java/com/jaronnie/springboot/controller/Credential.java
+++ springboot/src/main/java/com/jaronnie/springboot/controller/Credential.java
@@ -1,19 +1,29 @@
 package com.jaronnie.springboot.controller;
 
+import com.jaronnie.springboot.domain.bo.CredentialBo;
 import com.jaronnie.springboot.domain.vo.CredentialVo;
 import com.jaronnie.springboot.util.SerializationUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/v1.0/credential")
 public class Credential {
-    @RequestMapping("/{id}")
-    public String detail(@PathVariable int id) {
+    @GetMapping("/{id}")
+    public String detail(@PathVariable int id, HttpServletRequest request) {
+
+        System.out.println(request.getHeader("Content-Type"));
+        System.out.println(request.getHeader("version"));
+
         CredentialVo credentialVo =  new CredentialVo();
         credentialVo.setId(id);
         credentialVo.setName("jaronnieCredential");
         return SerializationUtils.serializeToCodeDataMessage(credentialVo);
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestBody CredentialBo credentialBo) {
+        System.out.println(credentialBo.getName());
     }
 }
---
+++ springboot/src/main/java/com/jaronnie/springboot/domain/bo/CredentialBo.java
@@ -0,0 +1,13 @@
+package com.jaronnie.springboot.domain.bo;
+
+public class CredentialBo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
