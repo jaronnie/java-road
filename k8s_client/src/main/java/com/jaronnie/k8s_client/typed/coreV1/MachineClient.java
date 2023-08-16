990a1c117c477f2014bba66fd3c293f647956e84
jaronnie <jaron@jaronnie.com>
2023-08-16 18:33:40 +0800

    feat(road): remove

--- k8s_client/src/main/java/com/jaronnie/k8s_client/typed/coreV1/MachineClient.java
+++
@@ -1,7 +0,0 @@
-package com.jaronnie.k8s_client.typed.coreV1;
-
-public class MachineClient {
-    public void list() {
-        System.out.println("list machine");
-    }
-}
--- springboot/src/main/java/com/jaronnie/springboot/controller/Hello.java
+++ springboot/src/main/java/com/jaronnie/springboot/controller/Hello.java
@@ -7,12 +7,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class Hello {
+    public static class Struct {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        private String name;
+        private int id;
+    }
+
     @RequestMapping("/say")
     public JSONObject say() {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("code",200);
         jsonObject.put("message", "ok");
-        jsonObject.put("data", "hello world");
+        Struct struct = new Struct();
+        struct.setId(1);
+        struct.setName("jaronnie");
+        JSONObject structJson = new JSONObject();
+        structJson.put("id", struct.id);
+        structJson.put("name", struct.name);
+        jsonObject.put("data", structJson);
 
         return jsonObject;
     }
