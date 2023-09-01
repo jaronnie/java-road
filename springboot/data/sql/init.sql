049248196a42d5b656a2bf057ab96935d61b2bef
jaronnie <jaron@jaronnie.com>
2023-09-01 11:21:29 +0800

    feat(springboot): update sql

--- springboot/data/sql/init.sql
+++ springboot/data/sql/init.sql
@@ -4,11 +4,13 @@ USE springboot;
 CREATE TABLE IF NOT EXISTS credential (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    type VARCHAR(255)
+    type VARCHAR(255),
+    create_time DATETIME,
+    update_time DATETIME
 );
 
-INSERT INTO credential (id, name, type)
-SELECT 1, 'jaronnie', 'k8s'
+INSERT INTO credential (id, name, type, create_time, update_time)
+SELECT 1, 'jaronnie', 'k8s', '2023-09-01 10:00:00', '2023-09-01 10:00:00'
 FROM DUAL
 WHERE NOT EXISTS (
     SELECT 1
