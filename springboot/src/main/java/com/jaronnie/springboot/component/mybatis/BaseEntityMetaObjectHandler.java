ccadef35d7b3ae1d7204652976553b6308ba9340
jaronnie <jaron@jaronnie.com>
2023-09-07 10:46:10 +0800

    feat(springboot): fix auto fill create_time and update_time

--- springboot/src/main/java/com/jaronnie/springboot/component/mybatis/BaseEntityMetaObjectHandler.java
+++ springboot/src/main/java/com/jaronnie/springboot/component/mybatis/BaseEntityMetaObjectHandler.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
@@ -12,9 +11,9 @@ public class BaseEntityMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        final LocalDateTime now = LocalDateTime.now();
-        strictInsertFill(metaObject, "createTime", LocalDateTime.class, now);
-        strictInsertFill(metaObject, "updateTime", LocalDateTime.class, now);
+        final Date now = new Date();
+        strictInsertFill(metaObject, "createTime", Date.class, now);
+        strictInsertFill(metaObject, "updateTime", Date.class, now);
     }
 
     @Override
--- springboot/src/main/java/com/jaronnie/springboot/component/mybatis/MybatisPlusConfig.java
+++ springboot/src/main/java/com/jaronnie/springboot/component/mybatis/MybatisPlusConfig.java
@@ -3,12 +3,14 @@ package com.jaronnie.springboot.component.mybatis;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class MybatisPlusConfig {
-    @Bean("paginationInterceptor")
+    @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
