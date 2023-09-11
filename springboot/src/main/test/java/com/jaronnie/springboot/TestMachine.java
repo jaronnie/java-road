8507e8e6972e0e46315e98789852b00c304790ce
jaronnie <jaron@jaronnie.com>
2023-09-11 10:24:52 +0800

    feat(test): 简化 get machine detatil 使用

--- springboot/src/main/test/java/com/jaronnie/springboot/TestMachine.java
+++ springboot/src/main/test/java/com/jaronnie/springboot/TestMachine.java
@@ -3,9 +3,11 @@ package com.jaronnie.springboot;
 import com.jaronnie.springboot.domain.vo.MachineStatisticsVo;
 import com.jaronnie.springboot.domain.vo.MachineVo;
 import com.jaronnie.springboot.util.httpUtil.ClientSet;
-import com.jaronnie.springboot.util.httpUtil.rest.RESTClient;;
+import com.jaronnie.springboot.util.httpUtil.rest.RESTClient;
 
 import java.util.ArrayList;
+
+;
 
 public class TestMachine {
 
@@ -27,11 +29,10 @@ public class TestMachine {
 
             System.out.println(machineStatisticsVos);
 
-            MachineVo machineVo = MachineVo.builder().build();
-            machineVo = clientSet.directClient()
+            MachineVo machineVo = clientSet.directClient()
                     .setVerb("GET")
                     .buildSubPath("/api/v1.0/machine/1")
-                    .call().into(machineVo.getClass());
+                    .call().into(MachineVo.class);
             System.out.println(machineVo);
 
         } catch (Exception e) {
