b64684811be3857359b22540fb9bd990b1b8e6ec
jaronnie <jaron@jaronnie.com>
2023-09-11 10:49:57 +0800

    feat(test): 修复 get machine statisticsVo

--- springboot/src/main/test/java/com/jaronnie/springboot/TestMachine.java
+++ springboot/src/main/test/java/com/jaronnie/springboot/TestMachine.java
@@ -1,5 +1,6 @@
 package com.jaronnie.springboot;
 
+import com.google.gson.reflect.TypeToken;
 import com.jaronnie.springboot.domain.vo.MachineStatisticsVo;
 import com.jaronnie.springboot.domain.vo.MachineVo;
 import com.jaronnie.springboot.util.httpUtil.ClientSet;
@@ -21,19 +22,20 @@ public class TestMachine {
         ClientSet clientSet = new ClientSet(restClient);
 
         try {
-            ArrayList<MachineStatisticsVo> machineStatisticsVos = new ArrayList<>();
-            machineStatisticsVos = clientSet.directClient()
+            ArrayList<MachineStatisticsVo> machineStatisticsVos = clientSet.directClient()
                     .setVerb("GET")
                     .buildSubPath("/api/v1.0/machine/statistics")
-                    .call().into(machineStatisticsVos.getClass());
+                    .call().into(new TypeToken<ArrayList<MachineStatisticsVo>>() {
+                    }.getType());
 
-            System.out.println(machineStatisticsVos);
+            System.out.println(machineStatisticsVos.get(0).getType());
 
             MachineVo machineVo = clientSet.directClient()
                     .setVerb("GET")
                     .buildSubPath("/api/v1.0/machine/1")
-                    .call().into(MachineVo.class);
-            System.out.println(machineVo);
+                    .call().into(new TypeToken<MachineVo>() {
+                    }.getType());
+            System.out.println(machineVo.getName());
 
         } catch (Exception e) {
             e.printStackTrace();
